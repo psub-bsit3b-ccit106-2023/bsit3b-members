85fe2c4cce4feded33f34cc0196f558824ad6778
Craig Ben Cadag <craigbencadag@gmail.com>
2023-01-31 09:38:03 +0800

    Add new personal information
    Craig Ben L. Cadag
    BSIT - 3B

---
+++ js/craig.js
@@ -0,0 +1,10 @@
+devs.push({
+
+    firstName : 'Craig Ben',
+    middleName: 'Lana',
+    lastName  : 'Cadag',
+    gender    : 'Male',
+    username  : 'CraigAndBen',
+    photo     : 'https://avatars.githubusercontent.com/CraigAndBen'
+
+});
