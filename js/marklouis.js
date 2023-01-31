2e36cae17c41546f0cddbf5f15628cb9c134c666
Mark Louis R. Odavar <122847155+Cintoshy@users.noreply.github.com>
2023-01-31 10:03:20 +0800

    Add Mark Louis Odavar

---
+++ js/marklouis.js
@@ -0,0 +1,9 @@
+devs.push({
+
+    firstName : 'Mark Louis',
+    middleName: 'Realina',
+    lastName  : 'Odavar',
+    gender    : 'Male',
+    username  : 'Cintoshy',
+    photo     : 'https://avatars.githubusercontent.com/u/122847155?s=400&v=4'
+});
