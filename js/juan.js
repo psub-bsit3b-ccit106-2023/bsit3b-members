c4147ec85539256d19347647575faa0e0e4af697
Juan Dela Cruz <sysdeveloper2021@gmail.com>
2023-01-31 10:03:03 +0800

    Add Juan Dela Cruz

---
+++ js/juan.js
@@ -0,0 +1,10 @@
+devs.push({
+
+    firstName : 'Juan',
+    middleName: 'Reyes',
+    lastName  : 'Dela Cruz',
+    gender    : 'Male',
+    username  : 'sysd3v',
+    photo     : 'https://avatars.githubusercontent.com/sysd3v'
+
+});
