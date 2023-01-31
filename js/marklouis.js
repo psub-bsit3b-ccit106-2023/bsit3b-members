f254411af27b008ff016a62fdaff8fec56d3df6f
Mark Louis R. Odavar <122847155+Cintoshy@users.noreply.github.com>
2023-01-31 10:08:13 +0800

    Update marklouis.js

--- js/marklouis.js
+++ js/marklouis.js
@@ -5,5 +5,5 @@ devs.push({
     lastName  : 'Odavar',
     gender    : 'Male',
     username  : 'Cintoshy',
-    photo     : 'https://avatars.githubusercontent.com/u/122847155?s=400&v=4'
+    photo     : 'https://avatars.githubuhttps://avatars.githubusercontent.com/u/122847155?s=400&u=e70361c1ced679daef9a20367f19cd72202eb78c&v=4sercontent.com/u/122847155?s=400&v=4'
 });
