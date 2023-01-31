19f8b35088098f15ea58ee103eac981f9c0bb3b9
Don John Daryl P. Curativo <doncurativo@my.cspc.edu.ph>
2023-01-31 09:26:30 +0800

    change to my name

---
+++ js/don.js
@@ -0,0 +1,10 @@
+devs.push({
+
+    firstName : 'Don John Daryl',
+    middleName: 'Paz',
+    lastName  : 'Curativo',
+    gender    : 'Male',
+    username  : 'Djohn6161',
+    photo     : 'https://avatars.githubusercontent.com/Djohn6161'
+
+});
